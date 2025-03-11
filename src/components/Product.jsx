37475de58605572b1e58096b2366478fd6f0628a
Renan Cavichi <renancavichi@gmail.com>
2025-03-11 17:00:26 -0300

    Aula Componentes Lista de Produtos

---
+++ src/components/Product.jsx
@@ -0,0 +1,41 @@
+import { Image } from 'expo-image'
+import {View, Text, StyleSheet} from 'react-native'
+
+export default function Product({foto, titulo, preco}) {
+    return (
+        <View style={styles.container}>
+            <Image 
+                style={styles.foto}
+                source={foto}
+            />
+            <Text style={styles.titulo}>{titulo}</Text>
+            <Text style={styles.preco}>{preco}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: 180,
+        padding: 15,
+        backgroundColor: '#DDD',
+        borderWidth: 1,
+        borderColor: '#CCC',
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    foto: {
+        width: 150,
+        height: 150,
+        borderRadius: 10
+    },
+    titulo: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    preco: {
+        fontSize: 14,
+        color: 'green'
+    }
+})
